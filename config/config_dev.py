e746cf13d0fdc33dd5db93575d95435de1283315
sameer khan <sameer@hexagonsearch.com>
2019-12-09 19:20:41 +0530

    config files

---
+++ config/config_dev.py
@@ -0,0 +1,3 @@
+APP_ENV = 'development'
+DEBUG = True
+FILE_PATH = "/home/sameer/litmusblox-backend/FileStore/processed_files/"
---
+++ config/config_test.py
@@ -0,0 +1,4 @@
+APP_ENV = 'development'
+TESTING = True
+DEBUG = True
+FILE_PATH = "/home/lbtest/serverApplication/FileStore/processed_files/"
