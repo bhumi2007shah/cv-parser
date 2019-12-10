59bea70f7181e940cdbdef06e472da98f69e12cf
sameer khan <sameer@hexagonsearch.com>
2019-12-10 17:46:27 +0530

    Added Name Extraction.

--- ExtractEmail.py
+++ ExtractEmail.py
@@ -5,6 +5,9 @@ def extract_email(email):
     email = re.findall("([^@|\s]+@[^@]+\.[^@|\s]+)", email)
     if email:
         try:
-            return email[0].split()[0].strip(';')
+            email = email[0].split()[0].strip(';')
+            email = email.replace("Email:", "")
+            email = email.replace("email:", "")
+            return email
         except IndexError:
             return None
--- ParseResume.py
+++ ParseResume.py
@@ -1,19 +1,16 @@
-import textract
-import config.config
-from datetime import datetime, date
 from Candidate import Candidate
 from EXtractSkills import extract_skills
 from ExtractEducation import extract_education
 from ExtractEmail import extract_email
 from ExtractMobile import extract_mobile_number
-from ExtractPdf import extract_text_from_pdf
+from ExtractName import extract_name
 from services.FileService import convert_to_text
 
 
 def extractData(parsed_text):
-    # print(extract_name(parsed_text))
     candidate = Candidate()
-    if len(extract_mobile_number(parsed_text))>0:
+    candidate.name = extract_name(parsed_text)
+    if len(extract_mobile_number(parsed_text)) > 0:
         candidate.mobile = extract_mobile_number(parsed_text).split(",")[0] if "," in extract_mobile_number(
             parsed_text) else extract_mobile_number(parsed_text)
         candidate.altMobile = extract_mobile_number(parsed_text).split(",")[1] if "," in extract_mobile_number(
