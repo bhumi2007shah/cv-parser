4c097b22b0bb72baafdda494a0f6337f1ac5f7cd
sameer khan <sameer@hexagonsearch.com>
2020-01-19 20:07:14 +0530

    changed condition and added absolute oath in startApp.sh

--- startApp.sh
+++ startApp.sh
@@ -4,15 +4,17 @@ then
   PATH=/home/sameer/litmusblox_cvParser
   ENV=dev
 else
-if [ $1 = "dev" ]; then
-  PATH=/home/sameer/litmusblox_cvParser
-  ENV=dev
-elif [ $1 = "test" ]; then
-  PATH=/home/lbtest/cvParserApplication
-  ENV=test
-elif [ $1 = "prod" ]; then
-  PATH=/home/lbprod/cvParserApplication
-  ENV=prod
+  if [ $1 = "dev" ]; then
+    PATH=/home/sameer/litmusblox_cvParser
+    ENV=dev
+  elif [ $1 = "test" ]; then
+    PATH=/home/lbtest/cvParserApplication
+    ENV=test
+
+  elif [ $1 = "prod" ]; then
+    PATH=/home/lbprod/cvParserApplication
+    ENV=prod
+  fi
 fi
 
 $PATH/config/config_$ENV.py $PATH/config/config.py
