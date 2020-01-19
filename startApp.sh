fca8f9ad362fcced3ed84ab3a524b2e2fcec8c02
sameer khan <sameer@hexagonsearch.com>
2020-01-19 20:21:17 +0530

    rolled back conditions as they were not working.

--- startApp.sh
+++ startApp.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-if [ -z "$1" ]
-then
-  PATH=/home/sameer/litmusblox_cvParser
-  ENV=dev
-else
-  if [ $1 = "dev" ]; then
-    PATH=/home/sameer/litmusblox_cvParser
-    ENV=dev
-  elif [ $1 = "test" ]; then
-    PATH=/home/lbtest/cvParserApplication
-    ENV=test
-
-  elif [ $1 = "prod" ]; then
-    PATH=/home/lbprod/cvParserApplication
-    ENV=prod
-  fi
-fi
-
-cp $PATH/config/config_$ENV.py $PATH/config/config.py
-
+cp config/config_$1.py config/config.py
 python3.6 app.py
