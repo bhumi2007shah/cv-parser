930e04f9edce5d3c6fc451879a7929d9eca2f295
sameer khan <sameer@hexagonsearch.com>
2020-01-19 19:51:25 +0530

    added filepath in prod env config file

--- config/config_prod.py
+++ config/config_prod.py
@@ -1,2 +1,4 @@
 FILE_PATH = ""
 APP_ENV = 'production'
+FILE_PATH = "/home/lbprod/serverApplication/FileStore/error_files/"
+PROCESSED_FILE_PATH = "/home/lbprod/serverApplication/FileStore/processed_files/"
