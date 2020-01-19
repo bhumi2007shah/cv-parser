e9d78bfe56be4b7dd68c4b0e8819c3e86e572e27
Ubuntu <lbprod@LBProd.hoocywrz1hjepkupzgikpwi3zd.bx.internal.cloudapp.net>
2020-01-19 13:53:49 +0000

    reverted chaange to startApp script and added condition if not param is passed as env.

--- startApp.sh
+++ startApp.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-cp config/config_$1.py config/config.py
+if [-z "$1"]
+then
+	cp config/config_dev.py config/confgi.py
+else
+	cp config/config_$1.py config/config.py
+fi
 python3.6 app.py
